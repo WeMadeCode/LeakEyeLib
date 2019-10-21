297878800bcb02f2354881300e91af872a82238c
zhouxiang <zhouxiang@huami.com>
2019-10-21 15:24:50 +0800

    升级Swift5.0

--- Example/LeakEyeLib/AppDelegate.swift
+++ Example/LeakEyeLib/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var eye = LeakEye()
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         
         self.eye.delegate = self
